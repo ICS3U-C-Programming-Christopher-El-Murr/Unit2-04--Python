e33fb1cabacfb8fb8250cf412cbe95bd4daad558
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-10-06 03:15:04 +0000

    added area_per_cicle.y

---
+++ area_per_circle.py
@@ -0,0 +1,20 @@
+#!.usr/bin/env python3
+# Created by Christopher El-Murr
+# Created on Oct,5,2025
+# This program calculates the circumference and area of a circle using pi
+
+import math
+
+
+def main():
+    import math
+
+
+rad = 8
+circ = math.pi * 2 * rad
+area = math.pi * rad**2
+print(circ, area)
+
+
+if __name__ == "__main__":
+    main()
